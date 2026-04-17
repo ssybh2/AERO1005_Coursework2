8c11bdcc2fdff77a639567bd2a75ae1cc7b9af9c
ssybh2 <ssybh2@nottingham.edu.cn>
2026-04-17 13:26:20 +0800

    0.5

--- Script_template.m
+++ Script_template.m
@@ -1,10 +1,18 @@
-% Insert name here
-% Insert email address here
-
+% Boyang Hu
+% ssybh2@nottingham.edu.cn
 
 %% PRELIMINARY TASK - ARDUINO AND GIT INSTALLATION [5 MARKS]
 
-% Insert answers here
+% Establish communication between MATLAB and Arduino
+a = arduino("COM3","Uno");
+
+% Blink one LED at 0.5 s intervals
+for i = 1:10
+    writeDigitalPin(a,"D8",1);
+    pause(0.5);
+    writeDigitalPin(a,"D8",0);
+    pause(0.5);
+end
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
 
