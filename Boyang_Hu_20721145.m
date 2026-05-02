6af30a611c71fc4cd52211890e2eac1054d345ff
ssybh2 <ssybh2@nottingham.edu.cn>
2026-05-02 18:09:42 +0800

    debug for short time

--- Boyang_Hu_20721145.m
+++ Boyang_Hu_20721145.m
@@ -30,7 +30,7 @@ sensor_pin = "A0";      % analogue input for MCP9700A
 
 % Use 600 for the real coursework submission
 
-duration = 600;
+duration = 6;
 
 date_string = input('Enter date in the format d/m/yyyy: ','s');
 location_string = input('Enter location: ','s');
