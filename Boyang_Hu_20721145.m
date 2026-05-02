fd9930476a1281eaff6693f2a761e6b461453be9
ssybh2 <ssybh2@nottingham.edu.cn>
2026-05-02 18:18:24 +0800

    blink test again

--- Boyang_Hu_20721145.m
+++ Boyang_Hu_20721145.m
@@ -12,12 +12,12 @@ else
 end
 
 % LED blink test
-% for i = 1:10
-%     writeDigitalPin(a,"D8",1);
-%     pause(0.5);
-%     writeDigitalPin(a,"D8",0);
-%     pause(0.5);
-% end
+ for i = 1:10
+     writeDigitalPin(a,"D8",1);
+     pause(0.5);
+     writeDigitalPin(a,"D8",0);
+     pause(0.5);
+ end
 
 
 %% TASK 1 - READ TEMPERATURE DATA, PLOT, AND WRITE TO A LOG FILE [20 MARKS]
